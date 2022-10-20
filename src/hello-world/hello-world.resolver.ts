80e7d83b9e7bc7d41d056b978a2ea9e384d46ecc
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-10-20 17:04:06 -0300

    feat: Args added

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,27 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    description: 'Hola mundo es lo que retorno',
+    name: 'hello',
+  })
   helloWorld(): string {
     return 'Hola Mundo';
+  }
+
+  @Query(() => Float, { name: 'ramdomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    name: 'randomFromZeroTo',
+    description: 'From to zero to argument TO (default 6)',
+  })
+  getRandomFromZeroTo(
+    @Args('to', { nullable: true, type: () => Int }) to = 6,
+  ): number {
+    return Math.floor(Math.random() * to);
   }
 }
